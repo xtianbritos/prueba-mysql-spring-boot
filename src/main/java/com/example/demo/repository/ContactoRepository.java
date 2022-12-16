91403697f9d00934a678e2bf33602b34ae36c9c0
Xtian <xtiancuenta1@gmail.com>
2022-12-16 09:30:49 -0300

    Agregado @Repository a los repositorios

--- src/main/java/com/example/demo/repository/ContactoRepository.java
+++ src/main/java/com/example/demo/repository/ContactoRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entities.Contacto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContactoRepository extends CrudRepository<Contacto, Integer> {
 }
--- src/main/java/com/example/demo/repository/TelefonoRepository.java
+++ src/main/java/com/example/demo/repository/TelefonoRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entities.Telefono;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TelefonoRepository extends CrudRepository<Telefono, Integer> {
 }
